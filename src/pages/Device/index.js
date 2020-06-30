dc633f2cf52c1f72536d7347b6cef747494f84db
hieunguyen2211 <phieu221199@gmail.com>
2020-06-30 14:32:20 +0700

    Finish Assistant Function

--- src/pages/Device/index.js
+++ src/pages/Device/index.js
@@ -1,213 +1,213 @@
-import React, { useState, useEffect } from "react";
-import ControlHeader from "../../components/Header/Control";
-import NavigationBar from "../../components/Navigation/NavigationBar";
-import RoomDetailCard from "../../components/Room/DetailCard";
-import { getAllLeds, updateStatusLed } from "../../firebase/devices/led";
-import { getAllServos, updateStatusServo } from "../../firebase/devices/servo";
-import SyncLoader from "react-spinners/SyncLoader";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import ControlHeader from '../../components/Header/Control';
+import NavigationBar from '../../components/Navigation/NavigationBar';
+import RoomDetailCard from '../../components/Room/DetailCard';
+import { getAllLeds, updateStatusLed } from '../../firebase/devices/led';
+import { getAllServos, updateStatusServo } from '../../firebase/devices/servo';
+import SyncLoader from 'react-spinners/SyncLoader';
+import './style.css';
 
 export default function DevicePage() {
-    const [loading, setLoading] = useState(true);
-    const [deviceData, setDeviceData] = useState([
+  const [loading, setLoading] = useState(true);
+  const [deviceData, setDeviceData] = useState([
+    {
+      id: 1,
+      name: 'Living room',
+      devices: [
         {
-            id: 1,
-            name: "Living room",
-            devices: [
-                {
-                    id: 1,
-                    name: "Light 1",
-                    iconName: "light",
-                    status: false,
-                },
-                {
-                    id: 2,
-                    name: "Light 2",
-                    iconName: "light",
-                    status: false,
-                },
-                {
-                    id: 3,
-                    name: "Main Door",
-                    iconName: "door",
-                    status: false,
-                },
-            ],
+          id: 1,
+          name: 'Light 1',
+          iconName: 'light',
+          status: false,
         },
         {
-            id: 2,
-            name: "Kitchen",
-            devices: [
-                {
-                    id: 1,
-                    name: "Light",
-                    iconName: "light",
-                    status: false,
-                },
-            ],
+          id: 2,
+          name: 'Light 2',
+          iconName: 'light',
+          status: false,
         },
         {
-            id: 3,
-            name: "Bedroom",
-            devices: [
-                {
-                    id: 1,
-                    name: "Light",
-                    iconName: "light",
-                    status: false,
-                },
-            ],
+          id: 3,
+          name: 'Main Door',
+          iconName: 'door',
+          status: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Kitchen',
+      devices: [
+        {
+          id: 1,
+          name: 'Light',
+          iconName: 'light',
+          status: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Bedroom',
+      devices: [
+        {
+          id: 1,
+          name: 'Light',
+          iconName: 'light',
+          status: false,
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: 'Bathroom',
+      devices: [
+        {
+          id: 1,
+          name: 'Light',
+          iconName: 'light',
+          status: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: 'Garage',
+      devices: [
+        {
+          id: 1,
+          name: 'Light',
+          iconName: 'light',
+          status: false,
         },
         {
-            id: 4,
-            name: "Bathroom",
-            devices: [
-                {
-                    id: 1,
-                    name: "Light",
-                    iconName: "light",
-                    status: false,
-                },
-            ],
+          id: 2,
+          name: 'Door',
+          iconName: 'door',
+          status: false,
         },
-        {
-            id: 5,
-            name: "Garage",
-            devices: [
-                {
-                    id: 1,
-                    name: "Light",
-                    iconName: "light",
-                    status: false,
-                },
-                {
-                    id: 2,
-                    name: "Door",
-                    iconName: "door",
-                    status: false,
-                },
-            ],
-        },
-    ]);
+      ],
+    },
+  ]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const ledReadAll = await getAllLeds();
-            const servoReadAll = await getAllServos();
-            const newDeviceData = deviceData;
-            newDeviceData.map((e) => {
-                let nameRoom = e.name.toUpperCase().replace(/\s/g, "");
-                e.devices.map((d) => {
-                    if (d.name.includes("Light")) {
-                        if (d.name === "Light 1") nameRoom = "LIVINGROOM1";
-                        if (d.name === "Light 2") nameRoom = "LIVINGROOM2";
-                        ledReadAll.map((room) => {
-                            if (room[0] === nameRoom)
-                                d.status = room[1].status === 0 ? false : true;
-                            return room;
-                        });
-                    } else if (d.name.includes("Door")) {
-                        let nameServo = d.name.toUpperCase().replace(/\s/g, "");
-                        if (nameRoom === "GARAGE") nameServo = "GARAGEDOOR";
-                        servoReadAll.map((servo) => {
-                            if (servo[0] === nameServo)
-                                d.status = servo[1].status === 0 ? false : true;
-                            return servo;
-                        });
-                    }
-                    return d;
-                });
-                return e;
+  useEffect(() => {
+    const fetchData = async () => {
+      const ledReadAll = await getAllLeds();
+      const servoReadAll = await getAllServos();
+      const newDeviceData = deviceData;
+      newDeviceData.map((e) => {
+        let nameRoom = e.name.toUpperCase().replace(/\s/g, '');
+        e.devices.map((d) => {
+          if (d.name.includes('Light')) {
+            if (d.name === 'Light 1') nameRoom = 'LIVINGROOM1';
+            if (d.name === 'Light 2') nameRoom = 'LIVINGROOM2';
+            ledReadAll.map((room) => {
+              if (room[0] === nameRoom)
+                d.status = room[1].status === 0 ? false : true;
+              return room;
             });
-            setDeviceData(newDeviceData);
-            setLoading(false);
-        };
-        fetchData();
-    }, [deviceData]);
-
-    const handleClickChangeStatus = (roomName, isLed) => {
-        let currentStatus = false;
-        if (isLed) {
-            deviceData.map((e) => {
-                let room_d = e.name;
-                e.devices.map((d) => {
-                    if (d.name === "Light 1") room_d = "livingroom1";
-                    if (d.name === "Light 2") room_d = "livingroom2";
-                    if (room_d === roomName) currentStatus = d.status;
-                    return d;
-                });
-                return e;
+          } else if (d.name.includes('Door')) {
+            let nameServo = d.name.toUpperCase().replace(/\s/g, '');
+            if (nameRoom === 'GARAGE') nameServo = 'GARAGEDOOR';
+            servoReadAll.map((servo) => {
+              if (servo[0] === nameServo)
+                d.status = servo[1].status === 0 ? false : true;
+              return servo;
             });
-
-            updateStatusLed(!currentStatus, roomName);
-            setDeviceData((deviceData) =>
-                deviceData.map((e) => {
-                    let room_d = e.name;
-                    e.devices.map((d) => {
-                        if (d.name === "Light 1") room_d = "livingroom1";
-                        if (d.name === "Light 2") room_d = "livingroom2";
-                        if (room_d === roomName) d.status = !currentStatus;
-                        return d;
-                    });
-                    return e;
-                })
-            );
-        } else {
-            const servoName = roomName;
-            deviceData.map((e) => {
-                let room_d = e.name;
-                e.devices.map((d) => {
-                    let nameServo = d.name;
-                    if (room_d === "Garage" && !d.name.includes("Light"))
-                        nameServo = "GARAGEDOOR";
-                    if (nameServo === servoName) currentStatus = d.status;
-                    return d;
-                });
-                return e;
-            });
-
-            updateStatusServo(!currentStatus, servoName);
-            setDeviceData((deviceData) =>
-                deviceData.map((e) => {
-                    let room_d = e.name;
-                    e.devices.map((d) => {
-                        let nameServo = d.name;
-                        if (room_d === "Garage" && !d.name.includes("Light"))
-                            nameServo = "GARAGEDOOR";
-                        if (nameServo === servoName) d.status = !currentStatus;
-                        return d;
-                    });
-                    return e;
-                })
-            );
-        }
+          }
+          return d;
+        });
+        return e;
+      });
+      setDeviceData(newDeviceData);
+      setLoading(false);
     };
+    fetchData();
+  }, [deviceData]);
 
-    return loading ? (
-        <div className="main-page-container">
-            <div
-                className="page-content-wrapper"
-                style={{ justifyContent: "center" }}
-            >
-                <SyncLoader size={20} color={"white"} loading={loading} />
-            </div>
+  const handleClickChangeStatus = (roomName, isLed) => {
+    let currentStatus = false;
+    if (isLed) {
+      deviceData.map((e) => {
+        let room_d = e.name;
+        e.devices.map((d) => {
+          if (d.name === 'Light 1') room_d = 'livingroom1';
+          if (d.name === 'Light 2') room_d = 'livingroom2';
+          if (room_d === roomName) currentStatus = d.status;
+          return d;
+        });
+        return e;
+      });
 
-            <NavigationBar />
-        </div>
-    ) : (
-        <div className="page-container">
-            <ControlHeader title="devices" path="/home" />
-            <div className="device-page-content-wrapper">
-                {deviceData.map((e) => (
-                    <RoomDetailCard
-                        key={e.id}
-                        roomName={e.name}
-                        deviceData={e.devices}
-                        onClick={handleClickChangeStatus}
-                    />
-                ))}
-            </div>
-            <NavigationBar />
-        </div>
-    );
+      updateStatusLed(!currentStatus, roomName);
+      setDeviceData((deviceData) =>
+        deviceData.map((e) => {
+          let room_d = e.name;
+          e.devices.map((d) => {
+            if (d.name === 'Light 1') room_d = 'livingroom1';
+            if (d.name === 'Light 2') room_d = 'livingroom2';
+            if (room_d === roomName) d.status = !currentStatus;
+            return d;
+          });
+          return e;
+        })
+      );
+    } else {
+      const servoName = roomName;
+      deviceData.map((e) => {
+        let room_d = e.name;
+        e.devices.map((d) => {
+          let nameServo = d.name;
+          if (room_d === 'Garage' && !d.name.includes('Light'))
+            nameServo = 'GARAGEDOOR';
+          if (nameServo === servoName) currentStatus = d.status;
+          return d;
+        });
+        return e;
+      });
+
+      updateStatusServo(!currentStatus, servoName);
+      setDeviceData((deviceData) =>
+        deviceData.map((e) => {
+          let room_d = e.name;
+          e.devices.map((d) => {
+            let nameServo = d.name;
+            if (room_d === 'Garage' && !d.name.includes('Light'))
+              nameServo = 'GARAGEDOOR';
+            if (nameServo === servoName) d.status = !currentStatus;
+            return d;
+          });
+          return e;
+        })
+      );
+    }
+  };
+
+  return loading ? (
+    <div className="main-page-container">
+      <div
+        className="page-content-wrapper"
+        style={{ justifyContent: 'center' }}
+      >
+        <SyncLoader size={20} color={'white'} loading={loading} />
+      </div>
+
+      <NavigationBar />
+    </div>
+  ) : (
+    <div className="page-container">
+      <ControlHeader title="devices" path="/home" />
+      <div className="device-page-content-wrapper">
+        {deviceData.map((e) => (
+          <RoomDetailCard
+            key={e.id}
+            roomName={e.name}
+            deviceData={e.devices}
+            onClick={handleClickChangeStatus}
+          />
+        ))}
+      </div>
+      <NavigationBar />
+    </div>
+  );
 }
--- src/pages/Main/Assistant.js
+++ src/pages/Main/Assistant.js
@@ -1,154 +1,260 @@
-import React, { useState } from "react";
-import ControlHeader from "../../components/Header/Control";
-import NavigationBar from "../../components/Navigation/NavigationBar";
-import { AudioFilled } from "@ant-design/icons";
-import { getCurrentLed, updateStatusLed } from "../../firebase/devices/led";
-import ChatBox from "../../components/ChatBox/ChatBox";
-import "./Assistant.css";
+import React, { useState, useEffect } from 'react';
+import ControlHeader from '../../components/Header/Control';
+import NavigationBar from '../../components/Navigation/NavigationBar';
+import { AudioFilled } from '@ant-design/icons';
+import { Input } from 'antd';
+import { getCurrentLed, updateStatusLed } from '../../firebase/devices/led';
+import {
+  getCurrentServo,
+  updateStatusServo,
+} from '../../firebase/devices/servo';
+import ChatBox from '../../components/ChatBox/ChatBox';
+import { API_CHATBOT_URL } from '../../api/config';
+import './Assistant.css';
 const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
+const urlKeyBoardIcon =
+  process.env.PUBLIC_URL + '/icons/assistant/keyboard.svg';
+const urlSendIcon = process.env.PUBLIC_URL + '/icons/assistant/send.svg';
 
 // recognition.start();
 
 function Devices() {
-    const [systemMessage, setSystemMessage] = useState("");
-    const [isRecording, setIsRecording] = useState(false);
-    const [message, setMessage] = useState("");
-    const voiceCommands = () => {
-        setMessage("");
-        setSystemMessage("");
-        setIsRecording(true);
-        // recognition.onstart = () => {
-        //     console.log('Voice is actived');
-        // };
-        recognition.onresult = async (e) => {
-            let current = e.resultIndex;
-            let transcript = e.results[current][0].transcript;
-            let mobileRepeatBug =
-                current === 1 && transcript === e.results[0][0].transcript;
-            if (!mobileRepeatBug) {
-                const message =
-                    transcript.charAt(0).toUpperCase() + transcript.slice(1);
-                setMessage(message);
-                if (transcript.search("light") !== -1) {
-                    try {
-                        let index = transcript.search("the");
-                        const newTranscript =
-                            transcript.slice(0, index) +
-                            transcript.slice(index + 3);
-                        index = newTranscript.search("the");
-                        if (index !== -1) {
-                            const roomName = newTranscript
-                                .slice(index + 3)
-                                .trim();
-                            const ledRead = await getCurrentLed(roomName);
-                            if (ledRead !== null) {
-                                const index = transcript.search("on");
-                                const statusInTranscript = index !== -1 ? 1 : 0;
-                                if (ledRead.status !== statusInTranscript) {
-                                    updateStatusLed(!ledRead.status, roomName);
-                                    setSystemMessage(
-                                        "Succeeded. Check your device"
-                                    );
-                                } else {
-                                    if (statusInTranscript === 1) {
-                                        setSystemMessage(
-                                            "The light is already turned on"
-                                        );
-                                    }
-                                    if (statusInTranscript === 0) {
-                                        setSystemMessage(
-                                            "The light is already turned off"
-                                        );
-                                    }
-                                }
-                            } else {
-                                setSystemMessage("Error command. Try again");
-                            }
-                        } else {
-                            setSystemMessage("Error command. Try again");
-                        }
-                    } catch (error) {
-                        setSystemMessage("Error command. Try again");
-                    }
-                } else {
-                    setSystemMessage("Error command. Try again");
-                }
-            }
-        };
-
-        setTimeout(() => {
-            recognition.start();
-        }, 50);
-
-        recognition.onspeechend = () => {
-            recognition.stop();
-            setIsRecording(false);
-        };
+  const [systemMessage, setSystemMessage] = useState('');
+  const [isRecording, setIsRecording] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+  const [userMessage, setUserMessage] = useState('');
+  const [userText, setUserText] = useState('');
+  const voiceCommands = () => {
+    setSystemMessage('');
+    setUserMessage('');
+    setIsRecording(true);
+    setIsTyping(false);
+    recognition.onresult = async (e) => {
+      let current = e.resultIndex;
+      let transcript = e.results[current][0].transcript;
+      if (transcript.length > 0) setUserMessage(transcript);
     };
 
-    return (
-        <div className="page-container">
-            <ControlHeader title="speech" path="/home" colorText="white" />
-            <div
-                className="page-content-wrapper"
-                style={{
-                    justifyContent: "space-between",
-                    top: "12vh",
-                    height: "74vh",
-                }}
-            >
-                <div style={{ height: "60vh" }}>
-                    <ChatBox
-                        avatar="/images/profile/assistant.jpg"
-                        message="How can I help you?"
-                    />
-                    {message !== "" && (
-                        <ChatBox
-                            avatar="/images/profile/avatar.jpg"
-                            message={message}
-                            reversed={true}
-                        />
-                    )}
-                    {systemMessage !== "" && (
-                        <ChatBox
-                            avatar="/images/profile/assistant.jpg"
-                            message={systemMessage}
-                        />
-                    )}
-                </div>
-                {!isRecording ? (
-                    <button
-                        onClick={() => voiceCommands()}
-                        className="btn-speech"
-                    >
-                        <AudioFilled
-                            style={{ color: "white", fontSize: "5vh" }}
-                        />
-                    </button>
-                ) : (
-                    <div id="bars">
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                    </div>
-                )}
-            </div>
+    setTimeout(() => {
+      recognition.start();
+    }, 50);
 
-            <br />
+    recognition.onspeechend = () => {
+      recognition.stop();
+      setIsRecording(false);
+    };
+  };
 
-            <NavigationBar />
+  const handleTextChange = (e) => {
+    setUserText(e.target.value);
+  };
+
+  const submitText = () => {
+    setUserMessage(userText);
+    setUserText('');
+    setIsTyping(false);
+  };
+
+  const handleCommand = async (command) => {
+    console.log(command);
+
+    if (command.length > 0) {
+      command = command.toLowerCase();
+      if (command.indexOf('light') !== -1) {
+        const statusRequestString = command.indexOf('on') !== -1 ? 'on' : 'off';
+        let roomName = command
+          .slice(
+            command.indexOf(statusRequestString) + statusRequestString.length,
+            command.indexOf('light')
+          )
+          .trim()
+          .replace(/ /g, '')
+          .toUpperCase();
+        if (roomName === 'LIVINGROOM') roomName = 'LIVINGROOM1';
+        const ledRead = await getCurrentLed(roomName);
+        const { status } = ledRead;
+        if (command.indexOf('on') !== -1) {
+          if (status === 1) {
+            setSystemMessage('This light is already turned on');
+          }
+          if (status === 0) {
+            updateStatusLed(1, roomName);
+            setSystemMessage('Succeeded. Check your device');
+          }
+        }
+        if (command.indexOf('off') !== -1) {
+          if (status === 0) {
+            setSystemMessage('This light is already turned off');
+          }
+          if (status === 1) {
+            updateStatusLed(0, roomName);
+            setSystemMessage('Succeeded. Check your device');
+          }
+        }
+      } else if (command.indexOf('door') !== -1) {
+        let servoName = '';
+        if (
+          command.indexOf('main') !== -1 ||
+          command.indexOf('living room') !== -1
+        ) {
+          servoName = 'MAINDOOR';
+        } else if (command.indexOf('garage') !== -1) {
+          servoName = 'GARAGEDOOR';
+        } else {
+          servoName = '';
+        }
+        if (servoName.length > 0) {
+          const servoRead = await getCurrentServo(servoName);
+          const { status } = servoRead;
+          if (command.indexOf('open') !== -1) {
+            if (status === 1) {
+              setSystemMessage('This door is already opened');
+            }
+            if (status === 0) {
+              updateStatusServo(1, servoName);
+              setSystemMessage('Succeeded. Check your device');
+            }
+          }
+          if (command.indexOf('close') !== -1) {
+            if (status === 0) {
+              setSystemMessage('This door is already closed');
+            }
+            if (status === 1) {
+              updateStatusServo(0, servoName);
+              setSystemMessage('Succeeded. Check your device');
+            }
+          }
+        } else {
+          setSystemMessage('Error command. Try again');
+        }
+      } else {
+        setSystemMessage(command);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const postData = () => {
+      fetch(API_CHATBOT_URL, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          sender: 'Hieu',
+          message: userMessage,
+        }),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          // if (res[0].text.length > 0) setSystemMessage(res[0].text);
+          if (res[0].text.length > 0) {
+            handleCommand(res[0].text);
+          }
+        })
+        .catch((error) => console.log(error));
+    };
+    if (userMessage.length > 0) {
+      postData();
+    }
+  }, [userMessage]);
+
+  return (
+    <div className="page-container">
+      <ControlHeader title="assistant" path="/home" colorText="white" />
+      <div
+        className="page-content-wrapper"
+        style={{
+          justifyContent: 'space-between',
+          top: '12vh',
+          height: '74vh',
+        }}
+      >
+        <div style={{ height: '60vh' }}>
+          <ChatBox
+            avatar="/images/profile/assistant.jpg"
+            message="How can I help you?"
+          />
+          {userMessage !== '' && (
+            <ChatBox
+              avatar="/images/profile/avatar.jpg"
+              message={
+                userMessage.charAt(0).toUpperCase() + userMessage.slice(1)
+              }
+              reversed={true}
+            />
+          )}
+          {systemMessage !== '' && (
+            <ChatBox
+              avatar="/images/profile/assistant.jpg"
+              message={
+                systemMessage.charAt(0).toUpperCase() + systemMessage.slice(1)
+              }
+            />
+          )}
         </div>
-    );
+        {!isRecording && !isTyping ? (
+          <div className="btn-bar">
+            <div
+              className="btn-assistant btn-speech"
+              onClick={() => voiceCommands()}
+            >
+              <AudioFilled />
+            </div>
+            <div
+              className="btn-assistant btn-keyboard"
+              onClick={() => {
+                setIsTyping(true);
+              }}
+            >
+              <img src={urlKeyBoardIcon} alt="icon" style={{ height: '4vh' }} />
+            </div>
+          </div>
+        ) : isRecording ? (
+          <div id="bars">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+        ) : (
+          <Input
+            className="assistant-text-area"
+            onChange={handleTextChange}
+            value={userText}
+            suffix={
+              userText.length > 0 ? (
+                <img
+                  src={urlSendIcon}
+                  alt="icon"
+                  style={{ height: '3vh' }}
+                  onClick={submitText}
+                />
+              ) : (
+                <AudioFilled
+                  style={{ fontSize: '3vh', color: '#204079' }}
+                  onClick={() => voiceCommands()}
+                />
+              )
+            }
+          />
+        )}
+      </div>
+      <br />
+      <NavigationBar />
+    </div>
+  );
 }
 
 export default Devices;
