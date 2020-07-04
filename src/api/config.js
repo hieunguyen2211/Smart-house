da2cf34655b16375a29d448da085a099f42b50e7
hieunguyen2211 <phieu221199@gmail.com>
2020-07-04 16:50:47 +0700

    update link chatbot config https -> http

--- src/api/config.js
+++ src/api/config.js
@@ -3,4 +3,4 @@ export const WEATHER_API_URL =
   'http://api.openweathermap.org/data/2.5/weather?q=Turan,VN&appid=1edc20a2043d744bfd967c394dfd4df8&units=metric';
 export const API_AI_URL = 'http://6238f448962f.ngrok.io';
 
-export const API_CHATBOT_URL = 'https://34.68.22.254/webhooks/rest/webhook';
+export const API_CHATBOT_URL = 'http://34.68.22.254/webhooks/rest/webhook';
