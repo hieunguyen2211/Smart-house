984091d7af0f5b04f3b0dea93855a03b9cb4c023
hieunguyen2211 <phieu221199@gmail.com>
2020-07-06 05:25:52 +0700

    update dotenv

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'antd/dist/antd.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as icons from './images/index.js';
 ReactDOM.render(<App icons={icons} />, document.getElementById('root'));
-
+require('dotenv').config();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
