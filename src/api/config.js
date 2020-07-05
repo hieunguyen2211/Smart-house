24ed1adda79d785d9ec84bd2cc355ebfc3e36d11
hieunguyen2211 <phieu221199@gmail.com>
2020-07-05 16:53:11 +0700

    update ai api url

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 export const API_URL = 'http://172.20.10.6:5000/api/v1';
 export const WEATHER_API_URL =
   'http://api.openweathermap.org/data/2.5/weather?q=Turan,VN&appid=1edc20a2043d744bfd967c394dfd4df8&units=metric';
-export const API_AI_URL = 'http://225ce3757b45.ngrok.io';
+export const API_AI_URL = 'http://0e3f44551206.ngrok.io';
 
 export const API_CHATBOT_URL = 'http://34.68.22.254/webhooks/rest/webhook';
