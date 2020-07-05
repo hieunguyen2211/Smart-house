fcab0dde49ba1ce51351e781a1f7f9f65b105717
hieunguyen2211 <phieu221199@gmail.com>
2020-07-06 05:29:28 +0700

    update

--- src/api/config.js
+++ src/api/config.js
@@ -2,6 +2,6 @@ export const API_URL = 'http://172.20.10.6:5000/api/v1';
 export const WEATHER_API_URL =
   'http://api.openweathermap.org/data/2.5/weather?q=Turan,VN&appid=1edc20a2043d744bfd967c394dfd4df8&units=metric';
 // export const API_AI_URL = 'http://15827b8758a1.ngrok.io';
-export const API_AI_URL = process.env.API_AI_URL;
+export const API_AI_URL = process.env.REACT_APP_API_AI_URL;
 
 export const API_CHATBOT_URL = 'http://34.68.22.254/webhooks/rest/webhook';
